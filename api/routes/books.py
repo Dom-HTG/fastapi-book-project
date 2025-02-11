55eb8b447783a21a74705736d29fa63bd6a3d3ac
Dominic <dominicdutchboy@gmail.com>
2025-02-11 19:34:01 +0100

    feat: add get_book_by_id route to fetch book details by ID && validation for book_id

--- api/routes/books.py
+++ api/routes/books.py
@@ -1,6 +1,6 @@
 from typing import OrderedDict
 
-from fastapi import APIRouter, status
+from fastapi import APIRouter, status, Depends, Path, HTTPException
 from fastapi.responses import JSONResponse
 
 from api.db.schemas import Book, Genre, InMemoryDB
@@ -32,6 +32,12 @@ db.books = {
     ),
 }
 
+def id_validator(book_id: str = Path(...)) -> int:
+    if not book_id.isdigit():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Book not found")
+
+    return int(book_id)
+
 
 @router.post("/", status_code=status.HTTP_201_CREATED)
 async def create_book(book: Book):
@@ -60,3 +66,13 @@ async def update_book(book_id: int, book: Book) -> Book:
 async def delete_book(book_id: int) -> None:
     db.delete_book(book_id)
     return JSONResponse(status_code=status.HTTP_204_NO_CONTENT, content=None)
+
+@router.get("/{book_id}", response_model=Book, status_code=status.HTTP_200_OK)
+async def get_book_by_id(book_id: int = Depends(id_validator)) -> Book:
+    try:
+        book = db.get_book(book_id)
+        if not book:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Book not found")
+        return book
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Book not found")
